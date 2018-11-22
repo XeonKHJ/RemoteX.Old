4b1d2dbfd60c3a5e817b690d880079002585d5e6
Hongjia <redalertkhj@live.cn>
2018-11-22 16:08:38 +0800

    Devide mouse funtion

--- RemoteX.Connection.BluetoothLE.Server/RemoteX.Connection.BluetoothLE.Server/RemoteFunction.cs
+++ RemoteX.Connection.BluetoothLE.Server/RemoteX.Connection.BluetoothLE.Server/RemoteFunction.cs
@@ -9,6 +9,7 @@ namespace RemoteX.Connection.BluetoothLE.Server
         enum ControlType {MouseMove, MouseClick, Keyboard};
         static public readonly Guid MouseMoveControl = new Guid("e1a3da24-0c8e-4935-b0a1-0cf579c867a2");
         static public readonly Guid KeyboardControl = new Guid("fbb7dbc9-9e35-465a-a2f8-cad2da11b583");
+        static public readonly Guid MouseClickControl = new Guid("c31e083e-a495-4b75-910c-04203e97753b");
 
         void RemoteFunction(BluetoothLESerivce service)
         {
@@ -18,7 +19,14 @@ namespace RemoteX.Connection.BluetoothLE.Server
         {
                 CharacteristicProperties = (GattCharacteristicProperties.Read)|(GattCharacteristicProperties.Notify),
                 ReadProtectionLevel = GattProtectionLevel.Plain,
-                UserDescription = "鼠标控制特征"
+                UserDescription = "鼠标移动特征"
+        };
+
+        GattLocalCharacteristicParameters mouseControlCharacteristicParameters = new GattLocalCharacteristicParameters
+        {
+                CharacteristicProperties = (GattCharacteristicProperties.Read)|(GattCharacteristicProperties.Notify),
+                ReadProtectionLevel = GattProtectionLevel.Plain,
+                UserDescription = "鼠标点击特征"
         };
         GattLocalCharacteristicParameters keyboardControlCharacteristicParameters = new GattLocalCharacteristicParameters
         {
